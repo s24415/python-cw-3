93af22a756e7e42a29f7b22375467dd56bd417b0
kondziet <konrad.dabrowski54@gmail.com>
2023-04-03 19:21:08 +0200

    Add rock-paper-scissors game functionality

--- main.py
+++ main.py
@@ -55,6 +55,70 @@ player2_name = "COMPUTER"
 if game_mode.upper() == "H" or game_mode.upper() == "HOT SEATS":
     player2_name = input("PLAYER2 NAME: ")
 
-for i in range(int(rounds_quantity)):
+def choose_winner(name1, name2, choice1, choice2):
+    choice1 = choice1.upper()
+    choice2 = choice2.upper()
+
+    if choice1 == "R" and choice2 == "R":
+        return "draw"
+    elif choice1 == "R" and choice2 == "P":
+        return name2
+    elif choice1 == "R" and choice2 == "S":
+        return name1
+    elif choice1 == "P" and choice2 == "R":
+        return name1
+    elif choice1 == "P" and choice2 == "P":
+        return "draw"
+    elif choice1 == "P" and choice2 == "S":
+        return name2
+    elif choice1 == "S" and choice2 == "R":
+        return name2
+    elif choice1 == "S" and choice2 == "P":
+        return name1
+    elif choice1 == "S" and choice2 == "S":
+        return "draw"
+
+round_counter = 1
+match_history = {}
+
+for i in range(rounds_quantity):
+    print("R -> ROCK | P -> PAPER | S -> SCISSORS")
     player1_choice = getpass.getpass("PLAYER1 CHOICE: ")
-    player2_choice = getpass.getpass("PLAYER2 CHOICE: ")
+    if game_mode.upper() == "C" or game_mode.upper() == "COMPUTER":
+        rand_number = random.randrange(3)
+        if rand_number == 0:
+            player2_choice = "R"
+        elif rand_number == 1:
+            player2_choice = "P"
+        elif rand_number == 2:
+            player2_choice = "S"
+    else:
+        player2_choice = getpass.getpass("PLAYER2 CHOICE: ")
+
+    match_history[round_counter] = {
+        player1_name: player1_choice,
+        player2_name: player2_choice,
+        "winner": choose_winner(player1_name, player2_name, player1_choice, player2_choice)
+    }
+    round_counter += 1
+
+player1_score = 0
+player2_score = 0
+for key in match_history:
+    winner = match_history[key]["winner"]
+    if winner == player1_name:
+        player1_score += 1
+    elif winner == player2_name:
+        player2_score += 1
+
+winner = "draw"
+if player1_score > player2_score:
+    winner = player1_name
+elif player2_score > player1_score:
+    winner = player2_name
+
+print (f"\n THE WINNER IS: {winner} \n")
+for key in match_history:
+    print(f"ROUND {key}")
+    print(f"{player1_name} -> {match_history[key][player1_name]}")
+    print(f"{player2_name} -> {match_history[key][player2_name]}")
